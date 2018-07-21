41c666bc94448b3dd7a99e6421a8d55511efa979
Hemant <hemantiitkcse@gmail.com>
2018-07-21 19:52:49 +0530

    first

--- server.js
+++ server.js
@@ -4,6 +4,9 @@ app.use(express.static(__dirname + '/build'));
 app.get('/',(req,res) => {
     res.sendFile(__dirname + '/build/index.html');
 })
+app.get('/loggedin',(req,res) => {
+    res.sendFile(__dirname + '/build/index.html')
+})
 app.listen(process.env.PORT || 3000,() => {
     console.log('App is Listening to' + process.env.PORT || 3000);
 })
