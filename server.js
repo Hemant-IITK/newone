6ab60dd142f09afcae2ea7652cba9477739e9acd
Hemant <hemantiitkcse@gmail.com>
2018-07-21 16:27:28 +0530

    first

---
+++ server.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const app = express();
+app.use(express.static(__dirname + '/build'));
+app.get('/',(req,res) => {
+    res.sendFile(__dirname + '/build/index.html');
+})
+app.listen(process.env.PORT || 3000,() => {
+    console.log('App is Listening to' + process.env.PORT || 3000);
+})
